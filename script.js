6e3185c5de0cd20b3d3256365f12977367000886
JustinLeeVance <justinleevance94@gmail.com>
2023-12-05 00:50:10 -0500

    Final commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 */
 const input = document.getElementsByClassName("linkToSite")[0];
+const changeFont = document.getElementsByClassName("bio");
 
 input.addEventListener('click', function(event) {
   input.style.color = "grey";
