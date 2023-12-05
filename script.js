036a9f407a41dc899c2fa0aa3a74f9f26d28a6f9
JustinLeeVance <justinleevance94@gmail.com>
2023-12-04 22:33:13 -0500

    added listenForEvent for lnk color change. Added open lnk in new window.

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 */
+const input = document.getElementsByClassName("linkToSite")[0];
 
-
+input.addEventListener('click', function(event) {
+  input.style.color = "grey";
+});
